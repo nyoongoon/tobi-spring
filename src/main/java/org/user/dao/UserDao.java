0f82eacd43d4ace56d1418e92485993382e26e52
yoon <nyoongoon@naver.com>
2023-08-14 08:29:49 +0900

    테스트 코드 개선

--- src/main/java/org/user/dao/UserDao.java
+++ src/main/java/org/user/dao/UserDao.java
@@ -34,16 +34,49 @@ public class UserDao {
         ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
-        rs.next();
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
+
+        User user = null;
+
+        if(rs.next()){ // 조회결과가 null이라도 코드가 진행될 수 있게 수정
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
 
         rs.close();
         ps.close();
         c.close();
 
+        if(user == null) throw new SQLDataException();
+
         return user;
     }
+
+    public void deleteAll() throws SQLException{
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement(("delete from uers"));
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+public int getCount() throws SQLException{
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return count;
+}
 }
--- src/main/java/org/user/dao/UserDaoTest.java
+++ src/main/java/org/user/dao/UserDaoTest.java
@@ -6,10 +6,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.user.domain.User;
 
+import java.sql.SQLDataException;
 import java.sql.SQLException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertThrows;
 
 
 public class UserDaoTest {
@@ -19,17 +21,55 @@ public class UserDaoTest {
                 new AnnotationConfigApplicationContext(DaoFactory.class);
 
         UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("gyumee", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
 
-        User user = new User();
-        user.setId("user");
-        user.setName("백기선");
-        user.setPassword("married");
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
 
-        dao.add(user);
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        User user2 = dao.get(user.getId());
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName(), is(user1.getName()));
+        assertThat(userget1.getPassword(), is(user1.getPassword()));
 
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassword(), is(user.getPassword()));
+        User userget2 = dao.get(user2.getId());
+        assertThat(userget2.getName(), is(user2.getName()));
+        assertThat(userget2.getPassword(), is(user2.getPassword()));
+    }
+
+    @Test
+    public void count() throws SQLException{
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("gyumee", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
+        User user3 = new User("bumjin", "박범진", "springno3");
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
+    }
+
+    @Test(expected = SQLDataException.class)
+    public void getUserFailuer() throws SQLException{
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.get("unknown_id");
     }
 }
